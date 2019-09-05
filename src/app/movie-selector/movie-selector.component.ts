c57f9c8a6f43b52e482fe802d63ee5abd2a80f7c
Barry Fleming II <b.fleming@trustetc.com>
2019-09-05 00:24:22 -0400

    Added Year search

--- src/app/movie-selector/movie-selector.component.ts
+++ src/app/movie-selector/movie-selector.component.ts
@@ -1,9 +1,7 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {FormControl, FormGroup} from '@angular/forms';
 import {Movie} from '../models/movie';
-import {Observable} from 'rxjs';
-import {map, startWith} from 'rxjs/operators';
-import {OmDbApiService} from '../amdbapi-service/om-db-api.service';
+import {OmDbApiService} from '../omdbapi-service/om-db-api.service';
 
 @Component({
   selector: 'app-movie-selector',
@@ -16,10 +14,6 @@ export class MovieSelectorComponent implements OnInit {
 
   @Output() moviesRequestEvent = new EventEmitter<Movie[]>();
   readonly years: number[] = MovieSelectorComponent._generateYears();
-  typeCtrl = new FormControl();
-  filteredTypes: Observable<string[]>;
-  yearCtrl = new FormControl();
-  filteredYears: Observable<number[]>;
   movieSelectForm = new FormGroup({
     movieTitle: new FormControl(''),
     movieType: new FormControl(),
@@ -27,12 +21,6 @@ export class MovieSelectorComponent implements OnInit {
   });
 
   constructor(private omdbApiService: OmDbApiService) {
-
-    this.filteredYears = this.yearCtrl.valueChanges
-      .pipe(
-        startWith(),
-        map(year => year ? this._filteredYears(year) : this.years.slice())
-      );
   }
 
   private static _generateYears(): number[] {
@@ -45,6 +33,7 @@ export class MovieSelectorComponent implements OnInit {
     }
     return years;
   }
+
   ngOnInit() {
   }
 
@@ -64,15 +53,4 @@ export class MovieSelectorComponent implements OnInit {
         console.error(error);
       }));
   }
-
-  private _filteredYears(value: number): number[] {
-    return this.years.filter(year => year.toString().indexOf(value.toString()) === 0);
-  }
-
-  private _filteredTypes(value: string): string[] {
-    const filterValue = value.toLowerCase();
-    return this.types.filter(type => type.toLowerCase().indexOf(filterValue) === 0);
-  }
-
-
 }
--- src/app/omdbapi-service/om-db-api.service.ts
+++ src/app/omdbapi-service/om-db-api.service.ts
@@ -1,16 +1,16 @@
-import { Injectable } from '@angular/core';
-import {HttpClient, HttpResponse} from '@angular/common/http';
-import {Movie} from '../models/movie';
-import {Observable} from 'rxjs';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 import {MovieListResponse} from '../models/MovieListResponse';
 
 const OMDB_API = 'http://www.omdbapi.com/?apikey=2ad4d3b5';
+
 @Injectable({
   providedIn: 'root'
 })
 export class OmDbApiService {
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {
+  }
 
   getOmdbApiData(params: string) {
     return this.httpClient.get<MovieListResponse>(OMDB_API + params);
